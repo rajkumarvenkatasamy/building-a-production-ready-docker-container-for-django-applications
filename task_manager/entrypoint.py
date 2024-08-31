c8c1d62e2ab8c912b18f9fb0b238d9aadcaba11d
Rajkumar Venkatasamy <rajkumar.venkatasamy@navis.com>
2024-08-31 12:34:49 +0530

    initial commit

--- task_manager/entrypoint.py
+++ task_manager/entrypoint.py
@@ -1,8 +1,12 @@
 import os
+import logging
+
 import uvicorn
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_manager.settings")
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
+    logging.info("The application will be served by Uvicorn via entrpoint module!!!")
     uvicorn.run("task_manager.asgi:application", host="0.0.0.0", port=8000, workers=1)
