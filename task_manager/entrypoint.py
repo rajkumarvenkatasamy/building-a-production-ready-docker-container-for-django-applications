0789cdf83dc84609f96376bab0af01b0981fb188
rajkumarvenkatasamy <venkatasamy.rajkumar@gmail.com>
2024-09-25 12:18:25 +0530

    Update entrypoint.py

--- task_manager/entrypoint.py
+++ task_manager/entrypoint.py
@@ -8,5 +8,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_manager.settings")
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    logging.info("The application will be served by Uvicorn via entrpoint module!!!")
+    logging.info("The application will be served by Uvicorn via entrypoint module!!!")
     uvicorn.run("task_manager.asgi:application", host="0.0.0.0", port=8000, workers=1)
